a01d3292ed3d5a564b51f4c99aae60767dc47f8b
Evgen Emel <e.evgeny999@gmail.com>
2023-11-20 12:31:01 +0300

    update

--- main.py
+++ main.py
@@ -1,31 +1,27 @@
+# Логгер
 from loguru import logger
-from dotenv import load_dotenv
-from os import environ, path
 
-load_dotenv(path.abspath('.env'))
+from os.path import abspath, join
+# Константы
+from constants import Constants
 
-FORMAT_LOGGER = environ.get('FORMAT_LOGGER')
-LEVEL_FILE_LOGGER = environ.get('LEVEL_FILE_LOGGER')
-LEVEL_CONSOLE_LOGGER = environ.get('LEVEL_CONSOLE_LOGGER')
-ROTATION_LOGGER = environ.get('ROTATION_LOGGER')
-SERIALIZE_LOGGER = environ.get('SERIALIZE_LOGGER') == 'True'
-
+constants = Constants()
 logger.add(
-    path.abspath(path.join('logs', '{time:YYYY-MM-DD  HH:mm:ss}.log')),  # Путь к файлу логов с динамическим именем
-    rotation=ROTATION_LOGGER,  # Ротация логов каждый день
+    abspath(join('logs', '{time:YYYY-MM-DD  HH.mm.ss}.log')),  # Путь к файлу логов с динамическим именем
+    rotation=constants.ROTATION_LOGGER,  # Ротация логов каждый день
     compression="zip",  # Использование zip-архива
-    level=LEVEL_FILE_LOGGER,  # Уровень логирования
-    format=FORMAT_LOGGER,  # Формат вывода
-    serialize=SERIALIZE_LOGGER,  # Сериализация в JSON
+    level=constants.LEVEL_FILE_LOGGER,  # Уровень логирования
+    format=constants.FORMAT_LOGGER,  # Формат вывода
+    serialize=constants.SERIALIZE_LOGGER,  # Сериализация в JSON
 )
 
 # # Вывод лога в консоль
 # logger.add(
 #     sink=print,
-#     level=LEVEL_CONSOLE_LOGGER,
-#     format=FORMAT_LOGGER,
+#     level=constants.LEVEL_CONSOLE_LOGGER,
+#     format=constants.FORMAT_LOGGER,
 # )
 
 
 if __name__ == '__main__':
-    pass
+    logger.info('RUN PROJECT')
