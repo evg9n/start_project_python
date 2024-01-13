4ec96d8d1c3cfd193dbcfa8293d90c973de69b58
evgen <e.evgeny999@gmail.com>
2024-01-13 14:43:34 +0300

    update constant.py

--- constants.py
+++ constants.py
@@ -15,10 +15,11 @@ class Constants:
         except FileNotFoundError:
             pass
 
-        self.FORMAT_LOGGER = environ.get('FORMAT_LOGGER')
-        self.LEVEL_FILE_LOGGER = environ.get('LEVEL_FILE_LOGGER')
-        self.LEVEL_CONSOLE_LOGGER = environ.get('LEVEL_CONSOLE_LOGGER')
-        self.ROTATION_LOGGER = environ.get('ROTATION_LOGGER')
+        self.FORMAT_LOGGER = (environ.get('FORMAT_LOGGER') or
+                              '{time:YYYY-MM-DD HH:mm:ss} | {level} | {file} | {message}')
+        self.LEVEL_FILE_LOGGER = environ.get('LEVEL_FILE_LOGGER') or 'DEBUG'
+        self.LEVEL_CONSOLE_LOGGER = environ.get('LEVEL_CONSOLE_LOGGER') or 'INFO'
+        self.ROTATION_LOGGER = environ.get('ROTATION_LOGGER') or '1 day'
         self.SERIALIZE_LOGGER = environ.get('SERIALIZE_LOGGER') == 'True'
 
     def __setattr__(self, name, value):
